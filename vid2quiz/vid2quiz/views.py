73005ca36be4c1c5aeaff60b5f04683ec2ec3171
Matt Mancini <matthewrmancini@gmail.com>
2018-04-08 03:32:12 -0400

    parses youtube url

--- vid2quiz/vid2quiz/views.py
+++ vid2quiz/vid2quiz/views.py
@@ -5,4 +5,8 @@ def entry_page(request):
 def summary_quiz_page(request):
     if request.method == "POST":
         print(request.POST)
+    string = ""
+    youtube_id = string[string.find("?v=")+3:]
     return render(request, 'summaryQuizPageText.html')
+def summary_quiz_page2(request):
+    return render(request, 'summaryQuizPage.html')
