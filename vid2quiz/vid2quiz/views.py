b8e6cc2f825ae46976e12e724222cdfffb424dbb
Matt Mancini <matthewrmancini@gmail.com>
2018-04-08 01:01:16 -0400

    puts html pages as templates

---
+++ vid2quiz/vid2quiz/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+def entry_page(request):
+    return render(request, 'entryPage.html')
+def summary_quiz_page(request):
+    return render(request, 'summaryQuizPage.html')
