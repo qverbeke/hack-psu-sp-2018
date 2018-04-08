4d8053a6e90de13d1709dfd2723e7cde35cd28a7
Matt Mancini <matthewrmancini@gmail.com>
2018-04-08 02:10:27 -0400

    updates views to have post

--- vid2quiz/vid2quiz/views.py
+++ vid2quiz/vid2quiz/views.py
@@ -3,4 +3,6 @@ from django.http import HttpRequest
 def entry_page(request):
     return render(request, 'entryPage.html')
 def summary_quiz_page(request):
+    if request.method == "POST":
+        print(request.POST)
     return render(request, 'summaryQuizPageText.html')
